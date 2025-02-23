abeb6ce1bc1c614ae71ef0c80d4f9605f70a4964
zineflix <zineflix0@gmail.com>
2025-02-23 21:45:51 +0800

    Update movie.js

--- ZineMax-main/1234/movie.js
+++ ZineMax-main/1234/movie.js
@@ -1,4 +1,14 @@
-    const apiKey = 'a1e72fd93ed59f56e6332813b9f8dcae'; // Your TMDB API Key
+// For Responsive Header
+window.addEventListener("scroll", function () {
+    let nav = document.querySelector("nav");
+    if (window.scrollY > 50) {
+        nav.classList.add("nav-solid"); // Solid color after scrolling down
+    } else {
+        nav.classList.remove("nav-solid"); // Transparent at the top
+    }
+});
+
+const apiKey = 'a1e72fd93ed59f56e6332813b9f8dcae'; // Your TMDB API Key
 const baseUrl = 'https://api.themoviedb.org/3';
 
 // Helper function to fetch movies and populate the row
